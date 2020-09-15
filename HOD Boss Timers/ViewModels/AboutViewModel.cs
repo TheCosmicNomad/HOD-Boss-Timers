fecd680419ce55e2febfe5d6c31428d823d4ec23
TheCosmicNomad <aidenfuller314@gmail.com>
2020-09-15 17:29:13 +1000

    Add project files.

---
+++ HOD Boss Timers/ViewModels/AboutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace HOD_Boss_Timers.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://xamarin.com"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
---
+++ HOD Boss Timers/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using HOD_Boss_Timers.Models;
+
+namespace HOD_Boss_Timers.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
